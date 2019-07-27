4ca6d70fc0f1dd15bceb2214e745dd249e992d5e
Hibaag1 <hibaag.rooble@hotmail.com>
2019-07-27 12:27:29 +0100

    fly code

---
+++ main.js
@@ -0,0 +1,28 @@
+//
+// // We're going to want to store the position of the character in here.
+// var state = {
+// character: {
+//   xPos: 100,
+//   yPos: 100,
+// }
+// };
+//
+// var canvas = document.querySelector("canvas");
+//
+// function runGame(game) {
+//   console.log(game);
+//
+//   // This function should be run repeatedly by a
+//   // timer so we can have animations
+//   function animate() {
+// game.drawTileToPos("character",0, state.character.xPos, state.character.yPos);
+//
+//     // All the drawing to the canvas should happen in here
+//   }
+// }
+//
+// StartGameEngine(canvas, config, runGame);
+//
+// window.addEventListener("keydown", function (event) {
+//   console.log("pressed a key: " + event.key);
+// });
